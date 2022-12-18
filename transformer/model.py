f67bc5724e9b5b0e72a266037c1bc3a1a9b27717
tro16 <tim.roethig@yahoo.de>
2022-12-18 09:09:50 +0100

    added alpha

---
+++ transformer/model.py
@@ -0,0 +1,8 @@
+from transformers import BertTokenizer, BertModel
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained("bert-base-uncased")
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
