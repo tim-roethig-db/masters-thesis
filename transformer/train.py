9bb52f006f2a9017d98f8a9dc913c1f82f68b13b
tro16 <tim.roethig@yahoo.de>
2023-01-09 14:26:20 +0100

    moved data to device

--- transformer/train.py
+++ transformer/train.py
@@ -44,6 +44,7 @@ if __name__ == '__main__':
             # extract input ids and attention mask squeeze to format (batch_size, max_bert_sequence_len)
             input_ids = x['input_ids'].squeeze(1).to(device)
             attention_mask = x['attention_mask'].squeeze(1).to(device)
+            y = y.to(device)
 
             # get prediction
             y_pred, _ = model(input_ids, attention_mask)
