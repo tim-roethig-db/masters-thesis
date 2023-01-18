e033e8444582e6f2054d6bcf84746fc54d7ee4ed
tim-roethig <tim.roethig@nordlb.de>
2023-01-18 22:24:00 +0100

    mem debugging

--- comb_model/train.py
+++ comb_model/train.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
             # get prediction
             y_pred, state = model(x_news_input_ids, x_news_attention_mask, x_price, news_feature_vect, state)
             print(y_pred)
-            #
+            #y_pred = torch.zeros((1, 1)).to(device)
             print(y_pred)
             #state = None
 
@@ -109,11 +109,9 @@ if __name__ == '__main__':
             epoch_monitor_loss += monitor_loss
 
             # perform gradient step
-            model.zero_grad()
-            batch_loss.backward()
-            optimizer.step()
-
-            y_pred = torch.zeros((1, 1)).to(device)
+            #model.zero_grad()
+            #batch_loss.backward()
+            #optimizer.step()
 
             p = 100
             if (batch_idx+1) % p == 0:
