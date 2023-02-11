37eabd4c88af9f9419dea37366220956c7c5b390
tro16 <tim.roethig@yahoo.de>
2023-02-11 12:37:34 +0100

    training

--- comb_model/train.py
+++ comb_model/train.py
@@ -9,14 +9,14 @@ from model import StockPriceModelRNN, StockPriceModelARN, StockPriceModelTransfo
 
 
 if __name__ == '__main__':
-    batch_size = 2
+    batch_size = 8
     lr = 0.001
     epochs = 10
-    n_news_features = 8
+    n_news_features = 16
     rnn_n_layers = 1
     rnn_hidden_size = 16
     seq_len = 10
-    lag = 1
+    lag = 0
 
     # set device to cuda if available
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
